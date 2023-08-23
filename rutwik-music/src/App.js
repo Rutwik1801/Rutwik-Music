61fc485162652ce5ea6b15c7208ca83460cefe34
Rutwik1801 <rutwik18012001@gmail.com>
2023-08-24 01:27:09 +0530

    lyrics corrected

--- rutwik-music/src/App.js
+++ rutwik-music/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Player from './Player';
 import Footer from './Footer';
@@ -14,10 +13,10 @@ import bekabooimg from "./covers/bekaboo.jpg"
 function App() {
   
 const lyrics={
-  khudkojaanlere:"Har pal lagta hai mai mai hu nahi mai hu koi aur,Kyu khudko dhooonde mai,Bazaaron me jaha sauda imaano ka,Toote khwabon ki ginti lekar khudko tolu mai,Toote sapno ko lekar raaton ko naa sou mai,Jitna paaya hai ekdin kho dunga ye sochu mai,Jaane kab se  iss bheed me khud ko akela pau mai,Peecha karte ye khwabon ko kaise bhagau mai",
-  beintehaa:"Aadhe se chaand ko takta rahu mai,poora ho tere chehre se,Duaon me maangu naa mere liye kuch, jo de wo rab sab tujhko,Aisi lagan tujh se laagi hai jo…na pehle mehsoos kiii thi,Dariya ki geheraai hai kuch nahi,Nadiyaan hai milti samundar me waise,Milte hai dil tere mere,Beintehaan,ishq gawaah,ishq khudaaa,Sehra me bhatka, musafir hu mai,Be mausam baarish hai tu,Band karu ye, aankhein meri,Khud me ho mehsoos tu,Aisi lagan tujh se laagi hai jo…na pehle mehsoos kiii thi,Dariya ki geheraai hai kuch nahi,Nadiyaan hai milti samundar me waise,Milte hai dil tere mere,Beintehaan,ishq gawaah,ishq khudaaa,Beintehaan...",
+  khudkojaanlere:"Har pal lagta hai mai mai hu nahi mai hu koi aur,khudko mai dhooondu inn,Bazaaron me jaha sauda imaano ka,Toote khwabon ki ginti lekar khudko tolu mai,Toote sapno ko lekar raaton ko naa sou mai,Kya hai iraada uparwaale ka koi samjha de,aake mujhko bhatka hu mai,Khud ko Jaan le re...",
+  beintehaa:"Aadhe se chaand ko takta rahu mai,poora ho tere chehre se,Duaon me maangu naa mere liye kuch, jo de wo rab sab tujhko,Aisi lagan tujh se laagi hai jo, pehle na mehsoos kiii thi,Nadiyaan hai milti samundar me jaise,Milte hai dil tere mere,Beintehaan,ishq gawaah,ishq khudaaa,Beintehaan,Sehra me bhatka, musafir hu mai,Be mausam baarish hai tu,Band karu ye, aankhein meri,Khud me ho mehsoos tu,Aisi lagan tujh se laagi hai jo,pehle na mehsoos kiii thi,Nadiyaan hai milti samundar me waise,Milte hai dil tere mere,Beintehaan,ishq gawaah,ishq khudaaa,Beintehaan...",
   manzoorhai:"Wo poori kar gayi aake kahaani ankahee,Tera hua sab kuch mera,Saari bachkaani meri harkatein sahi,Pyaar ki dori thaame dil mera,Meri iss rooh ka sauda manzoor hai,Mere iss dil kaaa sauda manzoor hai,Meri iss rooh ka sauda manzoor hai,Aadha tha waada jo poora manzoor hai...",
-  bekaboo:"Laakh,Koshishon seee,Dil meraaa,Hai thak chukaa,Kaiseee,Mai sambhaaluuu,Khudkooo,Ab yahaaa,Pyaar ke saaye me kaisi hai ranjisheinn,Naa kar saku ab mai bayaaannnn,Kya thi galti meriii jo ye silaa milaa Iiiishqq ka,Khudse hi ho gya judaaaa,Bekabooo,Hua jaa raha,Toot rahaaa,Sab Choot rahaaaa,Bekabooo,Hua jaa raha,Toot rahaaa,Sab Choot rahaaaa..."
+  bekaboo:"Laakh,Koshishon seee,Dil meraaa,thak chukaa,Kaiseee,Mai sambhaaluuu,Khudkooo,Ab yahaaa,Pyaar ke saaye me kaisi hai ranjisheinn,Naa kar saku ab mai bayaaan,Kya thi galti meri jo ye sila mila Ishq ka,Khudse hi ho gya judaa,Bekaboo,Hua jaa raha,Toot raha,Choot raha,Bekaboo,Hua jaa raha,Toot raha,Choot raha..."
 }
 
   return (
